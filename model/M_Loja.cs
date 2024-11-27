240e8bc4519e43ef302b918c7807c3487b71b3c1
Vinicius Maschio <vinicius.r.maschio@hotmail.com>
2024-11-27 11:14:19 -0300

    teste

--- model/M_Loja.cs
+++ model/M_Loja.cs
@@ -13,7 +13,7 @@ namespace Veterinaria.model
         public String nomeloja { get; set; }
         public M_Bairro bairro { get; set; }
         public M_Rua rua { get; set; }
-        public M_Cep cep { get; set; }    
+        public M_Cep cep { get; set; }  
         public M_Cidade cidade { get; set; }
         public M_Estado estado { get; set; }
         public M_Pais pais { get; set; }
